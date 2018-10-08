1a92fef9e4ec6ebeaa2887e53381f4ab41cec843
bryan.bedoya <bryan.bedoya@pragma.com.co>
2018-10-07 20:54:24 -0500

    Se agrega un test para consultar el listado de tipos de helado con su respectivo sabor

--- src/test/java/com/prueba/desarrollo/gaia/PruebaDesarrolloJavaApplicationTests.java
+++ src/test/java/com/prueba/desarrollo/gaia/PruebaDesarrolloJavaApplicationTests.java
@@ -1,16 +1,44 @@
 package com.prueba.desarrollo.gaia;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PruebaDesarrolloJavaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void getTiposHeladoList() throws Exception {
+        ResultActions resultActions = this.mvc.perform(get("/api/v1/tiposhelado").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        resultActions.andDo(print());
+    }
 
 }
